04626bb4b4bf7b32bc80cd0283c4851d809e5e24
Len Lanphar <lanphar@gmail.com>
2019-04-18 19:39:20 -0400

    added server startup script and form posting

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,29 @@
-from flask import render_template,send_from_directory
+from flask import render_template,send_from_directory,request,redirect
 from os import listdir
 from app import app
+import config
 
 VIDEO_DIR = "/home/pi/videos"
+
+CONFIG_FILE = "config.json"
 
 @app.route('/')
 def index():
     videos=[f for f in listdir(VIDEO_DIR) if f.endswith(".mp4")]
-    return render_template('index.html',videos=videos)
+    conf = config.load_config(CONFIG_FILE)
+    return render_template('index.html',videos=videos,config=conf)
+
+
+@app.route('/update',methods=['POST'])
+def update():
+    print("got here")
+    distance = request.form.get('distance')
+    print("got here 2")
+    units = request.form.get('units')
+    print(distance)
+    print(units)
+    return redirect("/")
+
 '''
 @app.route('/css/<path:path>')
 def send_css(path):
