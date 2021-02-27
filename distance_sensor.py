75c557f7c1c77c5e846c2d347828c468f2143c47
Len Lanphar <lanphar@gmail.com>
2021-02-27 13:54:50 -0500

    fixed max_reading calculation bug, clamped distance readings to MAX_DISTANCE

--- distance_sensor.py
+++ distance_sensor.py
@@ -2,6 +2,9 @@ import serial
 import logging
 import time
 
+# upper distance limit (cm)
+MAX_DISTANCE = 100
+
 class DistanceSensor:
     """
     Read and process US_100 sensor distance readings
@@ -23,6 +26,13 @@ class DistanceSensor:
         data = self.serial.read(2)
         cm = (data[1] + (data[0] << 8)) / 10
         logging.debug("raw distance: {:.1f} cm".format(cm))
+
+        # sometimes readings go abnormally high (e.g. in the thousands)
+        # there's probably a better way to handle this, but for now
+        # we'll just clamp it to a reasonable max value
+        cm = min(cm,MAX_DISTANCE)
+        logging.debug("clamped distance: {:.1f} cm".format(cm))
+
         return cm
 
     def get_distance(self):
@@ -41,10 +51,12 @@ class DistanceSensor:
             acc += reading
             if reading < min_reading:
                 min_reading = reading
-            elif reading > max_reading:
+            if reading > max_reading:
                 max_reading = reading
             time.sleep(0.005)
 
+        logging.debug("acc: {:.1f}, min_reading={:.1f}, max_reading={:.1f}".format(acc,min_reading,max_reading))
+            
         # remove min/max readings
         acc -= min_reading
         acc -= max_reading
