ca65a01e14c05aaf118b299c29e5875238620605
Len Lanphar <lanphar@gmail.com>
2021-12-13 15:22:23 -0500

    tkinter implementation

---
+++ cattube.py
@@ -0,0 +1,146 @@
+import logging
+import config
+import videos
+import distance_sensor
+import tkinter as tk
+from tkVideoPlayer import TkinterVideo
+import random
+
+LOG_FILE = "cattube.log"
+
+class View(tk.Frame):
+    """ Top-level application frame """
+
+    def __init__(self, parent, config, sensor, vids):
+        super().__init__(parent)
+
+        self.config = config
+        self.sensor = sensor
+        self.vids = vids
+
+        self.video_playing = False
+
+        self.play_clips = self.config.get("play_clips",False)
+        logging.info ("play_clips: {}".format(self.play_clips))
+        self.clip_duration = self.config.get("clip_duration",180)
+        if self.play_clips:
+            logging.info("clip_duration: {}".format(self.clip_duration))
+
+
+        # make window full screen
+        w,h = parent.winfo_screenwidth(), parent.winfo_screenheight()
+        parent.geometry("{0}x{1}+0+0".format(w,h))
+        # get rid of title bar, etc.
+        # parent.overrideredirect(1)
+        parent.config(bg="black")
+
+        # set up handlers for keypresses and mouse clicks
+        parent.bind("<Key>",self.key_pressed)
+        parent.bind("<Button>",self.mouse_click)
+
+        self.parent =parent 
+
+        # set up video player
+        self.video_player = TkinterVideo(master=parent, scaled=True, pre_load=False)
+        self.video_player.pack(expand=True, fill="both")
+
+        self.update()
+
+
+    def key_pressed(self,event):
+        self.parent.destroy()
+
+    def mouse_click(self,event):
+        self.parent.destroy() 
+
+    def update(self):
+        """ update sensor status """
+        active = self.sensor.update()
+        if active and not self.video_playing:
+            self.play_video()
+        self.after(100,self.update)
+
+
+    def video_end(self):
+        """ time to make video stop """
+        self.video_player.stop()
+        self.video_playing = False
+
+    def play_video(self):
+        """ find a video to play and start playing it """
+        video = self.vids.get_video()
+
+        video_duration = video['duration']
+
+        # default: play entire video
+        seek = 0.0
+        duration = video_duration
+
+        if self.play_clips and video_duration > self.clip_duration:
+            # pick a random section of the video to play
+            start = random.randint(0,video_duration-self.clip_duration)
+            duration = self.clip_duration
+            seek = float(start)
+
+        self.video_player.load(video['filename'])
+        self.video_player.seek(seek)
+
+        self.video_player.play()
+        self.video_playing = True
+
+        # set up callback to stop video play after duration
+        self.after(int(duration*1000),self.video_end)
+
+class CatTube(tk.Tk):
+    """ Main CatTube app class """
+    def __init__(self):
+        super().__init__()
+        
+        conf = config.CatTubeConfig()
+
+        # set up the sensor
+        kwargs = {}
+        sensor_device = conf.get("sensor_device")
+        if sensor_device is not None:
+            kwargs['serial_device'] = sensor_device
+
+        distance = conf.get("distance")
+        if distance is not None:
+            kwargs['distance'] = distance
+
+        if len(kwargs) == 0:
+            sensor = distance_sensor.DistanceSensor()
+        else:
+            sensor = distance_sensor.DistanceSensor(**kwargs)
+
+        video_dir = conf.get("video_dir")
+        if video_dir is None:
+            vids = videos.Videos()
+        else:
+            vids = videos.Videos(video_dir = video_dir)
+
+        # create view object(config,distance,video)
+        view = View(self,conf,sensor,vids)
+
+
+def main():
+
+    logging.basicConfig(filename=LOG_FILE,
+                        level=logging.INFO,
+                        format='%(asctime)s %(message)s')
+    logging.info("cattube starting")
+    
+    app = CatTube()
+    app.mainloop()
+
+    logging.info("cattube exiting")
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception as e:
+        logging.exception("main exception")
+    finally:
+        logging.info("Exiting")
+
--- videos.py
+++ videos.py
@@ -2,12 +2,10 @@ import random
 import logging
 from os import listdir,stat
 from os.path import join
-from subprocess import Popen
-import os
-import time
-import psutil
-import datetime
 from tinytag import TinyTag
+
+# default video dir
+VIDEO_DIR = "/home/pi/videos"
 
 class Videos:
     """
@@ -16,7 +14,7 @@ class Videos:
 
     player = None
     
-    def __init__(self,video_dir):
+    def __init__(self,video_dir = VIDEO_DIR):
         self.video_dir = video_dir
         self.last_video_dir_mtime = 0
         self.get_videos()
@@ -54,113 +52,3 @@ class Videos:
             self.videos.append(video)
         return self.videos
 
-    
-    def remove(self,video):
-        """
-        remove the video with the given filename
-        """
-        path = join(self.video_dir,video)
-        try:
-            os.remove(path)
-        except FileNotFoundError:
-            logging.error("File not found: "+path)
-
-
-    def _terminate_process(self,pid):
-        """
-        terminate a process and all its children
-        """
-        p = psutil.Process(pid)
-        children = p.children()
-        for child in children:
-            child.terminate()
-        gone, alive = psutil.wait_procs(children,timeout=3)
-        for child in alive:
-            # process still lingering; forefully kill it
-            logging("Child PID {} didn't terminate. Killing.".format(child.pid))
-            child.kill()
-        
-        p.terminate()
-        p.wait(timeout=3) # maybe this will address the zombie process problem?
-        # do a wait and p.kill() if process sticks around?
-
-            
-    def _omxplayer_timestamp(self,s):
-        # convert number of seconds to an omxplayer-style HH:MM:SS timestamp
-        ts = datetime.timedelta(seconds=s)
-        return str(ts)
-
-    
-    def play_video(self,mute=False,clip_duration=0):
-        """
-        select a random video and play it
-        """
-
-        play_clips = clip_duration > 0
-
-        # get a random video to play
-        video = self.get_video()
-        video_file = video['filename']
-        
-        args = ['/usr/bin/omxplayer','-b','--no-osd']
-
-        if mute:
-            # not sure how reliable omxplayer -n -1 is, but not
-            # sure of any better alternatives for muting
-            args.extend(["-n","-1"])
-
-
-        video_duration = int(video['duration'])
-
-        # default: play entire video
-        timestamp = self._omxplayer_timestamp(0)
-        duration = video_duration
-    
-        if play_clips and video_duration > clip_duration:
-            # pick a random section of the video to play
-            start = random.randint(0,video_duration-clip_duration)
-            duration = clip_duration
-            timestamp = self._omxplayer_timestamp(start)
-            args.extend(["--pos",timestamp])
-
-        args.append(video_file)
-
-        logging.info("playing {}@{}".format(video_file,timestamp))
-
-        try:
-            self.player = Popen(args)
-        except Exception:
-            logging.exception("process creation failed: ")
-            logging.info(args)
-            return None
-
-        self.player_duration = duration
-        self.player_start = time.time()
-
-        return video
-
-    
-    def is_finished(self):
-        """
-        Check if video is done playing
-        """
-        now = time.time()
-        finished = False
-        if self.player is None:
-            finished = True
-        elif self.player.poll() is not None:
-            # player process has finished
-            finished = True
-        elif now >= self.player_start + self.player_duration:
-            # player still running, clip end time reached
-            self._terminate_process(self.player.pid)
-            finished = True
-
-        if finished:
-            # reset everything
-            self.player = None
-            self.player_start = 0
-            self.player_duration = 0
-        
-        return finished
-    
